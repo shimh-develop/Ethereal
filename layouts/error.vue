42a15e5ca436dfbc48c9b6379f4cf0f03b72d7c5
shiminghui <shiminghui>
2019-08-15 18:07:23 +0800

    全局错误页面

---
+++ layouts/error.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container grid-list-xl style="margin-top: 80px">
+    <v-layout
+      align-center
+      justify-center
+    >
+      <v-flex xs10>
+        <v-alert
+          :value="true"
+          color="error"
+          icon="warning"
+          outline
+        >
+          页面不存在，跳转中 {{ process }}
+        </v-alert>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        process: ''
+      }
+    },
+    beforeMount() {
+      let i = 0
+      setInterval(() => {
+        i++
+        this.process += '......'
+        i === 3 &&  this.$router.push('/')
+      }, 1000)
+    }
+  }
+</script>
